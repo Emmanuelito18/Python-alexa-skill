c4f9cbc54b8496e4780f04e42dc35ff32915d4e4
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-04-17 20:50:45 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -28,7 +28,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         #speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
-        speak_output= "Esto es una prueba de mi skill de alexa, llamada mayordomo servicial creada por: Emmanuelito18"
+        speak_output= "Esto es una prueba de mi skill de alexa, llamada mayordomo servicial, creada por: Emmanuelito18"
 
         return (
             handler_input.response_builder
